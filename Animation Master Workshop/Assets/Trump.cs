590719974e9e423b9af596bf1beafada4b3d5fb5
Taher Jerbi <jerbi.taherr@gmail.com>
2020-03-13 18:41:00 +0100

    init

---
+++ Animation Master Workshop/Assets/Trump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trump : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        //  Make sure all the transition have 0 exit time
+
+        animator.SetBool("Up", Input.GetKeyDown(KeyCode.UpArrow));
+        animator.SetBool("Down", Input.GetKeyDown(KeyCode.DownArrow));
+        animator.SetBool("Right", Input.GetKeyDown(KeyCode.RightArrow));
+        animator.SetBool("Left", Input.GetKeyDown(KeyCode.LeftArrow));
+
+
+        // You can use trigger to achieve the same effect
+    }
+}
